5364202ed32afb8588e9d0aa4f89665cd683a443
boozedog <code@booze.dog>
2025-11-24 06:40:22 -0500

    vscodium, orbstack

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x SSH_AUTH_SOCK ~/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
